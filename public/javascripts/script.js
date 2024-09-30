4e75c4bc362240fe53df114b7d167d0db6030906
AC_Marvel <gil_1_10@hotmail.com>
2024-10-01 01:05:40 +0200

    Conclusão mais de um projeto de treinamento de JavaScript, HTML e CSS

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,19 +4,35 @@ window.onload = function() {
     pack.addEventListener("click", openPack);
 }
 
+function randomNumber(min, max) {
+    return Math.ceil(Math.random() * (max-min) + min);
+}
+
 function openPack() {
     // alert("card pack open");
+    let cardsOpened = document.getElementById("pokemon-cards-opened");
+    while (cardsOpened.firstChild) {
+        cardsOpened.firstChild.remove();
+    }
+    
     for (let i = 0; i < 11; i++) {
         /*<div class="pokemon-card">
              <img id="0" src="./pokemon-cards/base set (10).jpg">
         </div> */
-        let cardDiv = docuemnt.createElement("div");
+        let cardDiv = document.createElement("div");
         cardDiv.classList.add("pokemon-card");
 
         let cardImg = document.createElement("img");
         cardImg.id = i;
 
-        cardImg.src = "./images/base set (" + "10" + ").jpg";
+        let num = 1;
+        if (i == 10) {
+            num = randomNumber(1, 16);
+        }
+        else {
+            num = randomNumber(17, 102);
+        }
+        cardImg.src = "./images/base set ("+ num.toString() + ").jpg";
 
         cardDiv.appendChild(cardImg);
         document.getElementById("pokemon-cards-opened").appendChild(cardDiv);
